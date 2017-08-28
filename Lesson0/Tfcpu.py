18df278179e5bf60bf8935f2eff139934e70334c
unknown <kevinahoefle@gmail.com>
2017-08-28 10:41:16 +0200

    VT fix

--- Lesson0/Tfcpu.py
+++ Lesson0/Tfcpu.py
@@ -4,7 +4,8 @@ This script will do a simple matrix multiplication
 It will be executed on the CPU
 
 """
-import tensorflow as tf with tf.device('/cpu:0'): 
+import tensorflow as tf 
+with tf.device('/cpu:0'): 
    a = tf.constant([4.0, 4.0, 5.0, 2.0, 3.0, 5.0], shape=[2, 3], name='a') 
    b = tf.constant([5.0, 6.0, 6.0, 1.0, 3.0, 4.0], shape=[3, 2], name='b') 
    c = tf.matmul(a, b) 
