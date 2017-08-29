0a03d9c73a1643ed500c6024688ef3034f0fa343
unknown <kevinahoefle@gmail.com>
2017-08-29 10:21:35 +0200

    lesson3

---
+++ Lesson3/constAddParamVarDivWithWriter.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 28 17:13:01 2017
+
+@author: khoefle
+"""
+
+
+##Imports the Tensorflow library
+import tensorflow as tf
+
+with tf.device('/gpu:0'): #with tf.device('/cpu:0'):
+    #Define the constant, with a value, datatype, and a representive name
+    nc1 = tf.constant(10,tf.float32,name='const1')
+    nc2 = tf.constant(11,tf.float32,name='const2')
+    
+    #Define the place holder x
+    x = tf.placeholder(tf.float32,name='x')
+    
+    #Define the variable y
+    y = tf.Variable(5.0,tf.float32,name='y')
+    
+    #Describe the subtraction
+    ns = x - y;
+    
+    #Describe the addition
+    na = tf.add(nc1,nc2)
+    
+    #Describe the divison
+    nd = tf.divide(na,ns)
+    
+    #Create the Session object   
+    with tf.Session() as session:
+        wr = tf.summary.FileWriter('./simpleExample',session.graph)
+        
+        
+        #Initialize variables
+        init = tf.global_variables_initializer()
+        
+        #parse to session
+        session.run(init)
+        print(session.run(nd,{x:7.0}))
+        
+        
